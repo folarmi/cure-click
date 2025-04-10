676c2467f5009ee1bb3e729269f9b44fe602c19b
Folacodes <folacodes@Folacodess-MacBook-Pro.local>
2025-04-10 12:50:54 +0100

    adjusting ui

--- src/pages/appointment/Calendar.tsx
+++ src/pages/appointment/Calendar.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Box, Button, Flex, Text } from "@radix-ui/themes";
+import { Box, Button, Flex, IconButton, Switch, Text } from "@radix-ui/themes";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { DayScheduleItem } from "./DaySchedule";
@@ -16,7 +16,8 @@ import {
   getTimeZoneInfo,
 } from "../../utils/util";
 import { CustomCheckBox } from "../../components/ui/CustomCheckBox";
-import { sessionsData } from "../../utils/data";
+import { availableTimes, sessionsData } from "../../utils/data";
+import { PlusIcon, TrashIcon } from "@radix-ui/react-icons";
 
 const Calendar = () => {
   const [expandedDay, setExpandedDay] = useState<string | null>(null);
@@ -84,41 +85,191 @@ const Calendar = () => {
       {doctorAvailableSessionsIsLoading || doctorProfileIsLoading ? (
         <Loader />
       ) : (
-        <Flex direction="column" gap="4" className="p-4">
-          <Text size="5" weight="bold">
-            Weekly Availability
-          </Text>
+        <Flex className="mt-10">
+          <Box>
+            <Text as="p" size="4" className="text-gray12 font-semibold">
+              Availability Hours
+            </Text>
+            <Text
+              as="p"
+              size="3"
+              className="text-gray11 font-medium pt-1 w-[316px]"
+            >
+              Setup your Schedule to assist patients know when to book sessions
+            </Text>
 
-          <Box className="border rounded-lg overflow-hidden">
-            {sessionsData?.map((day: DaySchedule) => (
-              <DayScheduleItem
-                key={day.dayOfTheWeek}
-                day={day}
-                control={control}
-                watch={watch}
-                isExpanded={expandedDay === day.publicId}
-                onToggle={(id) =>
-                  setExpandedDay(id === expandedDay ? null : id)
-                }
-                setValue={setValue}
-              />
-            ))}
+            <Box className="border rounded-lg overflow-hidden">
+              {sessionsData?.map((day: DaySchedule) => (
+                <DayScheduleItem
+                  key={day.dayOfTheWeek}
+                  day={day}
+                  control={control}
+                  watch={watch}
+                  isExpanded={expandedDay === day.publicId}
+                  onToggle={(id) =>
+                    setExpandedDay(id === expandedDay ? null : id)
+                  }
+                  setValue={setValue}
+                />
+              ))}
+            </Box>
+
+            {expandedDay && (
+              <div className="flex items-center my-4">
+                <CustomCheckBox
+                  name={`schedule.${expandedDay}.recurring`}
+                  control={control}
+                />
+                <Text
+                  size="2"
+                  className="text-text pl-2"
+                  weight="regular"
+                  as="p"
+                >
+                  Make Recurring
+                </Text>
+              </div>
+            )}
+
+            <Button
+              className="bg-grass9 text-base font-medium mb-4"
+              loading={updateDoctorAvailableSessionMutation.isPending}
+              disabled={updateDoctorAvailableSessionMutation.isPending}
+              onClick={submitAvailableSessions}
+            >
+              Save Schedule{" "}
+            </Button>
           </Box>
 
-          {expandedDay && (
-            <div className="flex items-center my-4">
-              <CustomCheckBox
-                name={`schedule.${expandedDay}.recurring`}
-                control={control}
-              />
-              <Text size="2" className="text-text pl-2" weight="regular" as="p">
-                Make Recurring
-              </Text>
-            </div>
-          )}
-          <Button onClick={handleSubmit(submitAvailableSessions)}>
-            Save Schedule
-          </Button>
+          <Flex direction="column" align="center" className=" w-full mx-auto">
+            <Text as="p" size="4" className="text-gray12 font-semibold">
+              Blockout Dates
+            </Text>
+
+            <Text
+              as="p"
+              size="3"
+              className="text-gray11 font-medium pt-1 w-[546px]"
+            >
+              Add days you do not want to get bookings. This will be applied to
+              all sessions immediately.
+            </Text>
+
+            <Box className="mt-4 border border-gray3  w-[564px]">
+              <div className="px-4 py">
+                {availableTimes?.slice(0, 1).map(({ day, id, status }) => {
+                  return (
+                    <Box>
+                      <Flex
+                        key={id}
+                        align="center"
+                        justify="between"
+                        className="py-3 border-b border-gray3"
+                      >
+                        <Text as="p" weight="medium" size="2">
+                          {day}
+                        </Text>
+                        <Text
+                          as="p"
+                          weight="regular"
+                          size="2"
+                          className="text-left"
+                        >
+                          {status}
+                        </Text>
+                        <Switch
+                          variant="soft"
+                          size="2"
+                          checked={isBlockedOutDaysSwitchEnabled}
+                          onCheckedChange={handleBlockedOutSwitchChange}
+                        />
+                      </Flex>
+
+                      {isBlockedOutDaysSwitchEnabled && (
+                        <>
+                          <Flex
+                            align="center"
+                            justify="between"
+                            className="my-4"
+                          >
+                            <Flex key={id} align="center">
+                              <Select.Root size="1">
+                                <Select.Trigger
+                                  placeholder="Wed, 12 Dec 2024"
+                                  className="w-[140px]"
+                                />
+                                <Select.Content variant="soft">
+                                  <Select.Group>
+                                    <Select.Label>Fruits</Select.Label>
+                                    <Select.Item value="orange">
+                                      Orange
+                                    </Select.Item>
+                                    <Select.Item value="apple">
+                                      Apple
+                                    </Select.Item>
+                                    <Select.Item value="grape" disabled>
+                                      Grape
+                                    </Select.Item>
+                                  </Select.Group>
+                                </Select.Content>
+                              </Select.Root>
+                              <Text
+                                as="p"
+                                size="1"
+                                weight="regular"
+                                className="px-6"
+                              >
+                                To
+                              </Text>
+                              <Select.Root size="1">
+                                <Select.Trigger
+                                  className="w-[140px]"
+                                  placeholder="Mon, 17 Dec 2024"
+                                />
+                                <Select.Content>
+                                  <Select.Group>
+                                    <Select.Label>Fruits</Select.Label>
+                                    <Select.Item value="orange">
+                                      Orange
+                                    </Select.Item>
+                                    <Select.Item value="apple">
+                                      Apple
+                                    </Select.Item>
+                                    <Select.Item value="grape" disabled>
+                                      Grape
+                                    </Select.Item>
+                                  </Select.Group>
+                                </Select.Content>
+                              </Select.Root>
+                            </Flex>
+
+                            <IconButton
+                              style={{
+                                border: "1px solid var(--border-gray)",
+                              }}
+                              className="bg-transparent text-neutral_11"
+                              size="1"
+                            >
+                              <TrashIcon />
+                            </IconButton>
+                          </Flex>
+                          <Button
+                            style={{
+                              border: "1px solid var(--border-gray)",
+                            }}
+                            size="1"
+                            className="text-sm font-medium bg-transparent text-neutral_11 mb-4"
+                          >
+                            <PlusIcon /> Add Blockout Dates
+                          </Button>
+                        </>
+                      )}
+                    </Box>
+                  );
+                })}
+              </div>
+            </Box>
+          </Flex>
         </Flex>
       )}
     </>
@@ -203,3 +354,150 @@ export { Calendar };
 )}
 </Box> */
 }
+
+<Flex className="mt-10">
+  <Box>
+    <Text as="p" size="4" className="text-gray12 font-semibold">
+      Availability Hours
+    </Text>
+    <Text as="p" size="3" className="text-gray11 font-medium pt-1 w-[316px]">
+      Setup your Schedule to assist patients know when to book sessions
+    </Text>
+
+    <Box className="mt-4 border border-gray3  w-[564px]">
+      {doctorAvailableSessions?.data?.map(
+        ({ dayOfTheWeek, publicId, localTimes }) => (
+          <DayScheduleItem
+            key={publicId}
+            dayOfTheWeek={dayOfTheWeek}
+            publicId={publicId}
+            defaultPeriods={localTimes}
+            isExpanded={expandedDay === publicId}
+            onToggle={handleToggleDay}
+            control={control}
+            watch={watch}
+          />
+        )
+      )}
+    </Box>
+
+    <div className="flex items-center my-4">
+      <Checkbox className="data-[state=checked]:bg-red-500" />
+      <Text size="2" className="text-text pl-2" weight="regular" as="p">
+        Make Recurring
+      </Text>
+    </div>
+    <Button
+      className="bg-grass9 text-base font-medium mb-4"
+      loading={updateDoctorAvailableSessionMutation.isPending}
+      disabled={updateDoctorAvailableSessionMutation.isPending}
+      onClick={submitAvailableSessions}
+    >
+      Save Schedule{" "}
+    </Button>
+  </Box>
+
+  <Flex direction="column" align="center" className=" w-full mx-auto">
+    <Text as="p" size="4" className="text-gray12 font-semibold">
+      Blockout Dates
+    </Text>
+
+    <Text as="p" size="3" className="text-gray11 font-medium pt-1 w-[546px]">
+      Add days you do not want to get bookings. This will be applied to all
+      sessions immediately.
+    </Text>
+
+    <Box className="mt-4 border border-gray3  w-[564px]">
+      <div className="px-4 py">
+        {availableTimes?.slice(0, 1).map(({ day, id, status }) => {
+          return (
+            <Box>
+              <Flex
+                key={id}
+                align="center"
+                justify="between"
+                className="py-3 border-b border-gray3"
+              >
+                <Text as="p" weight="medium" size="2">
+                  {day}
+                </Text>
+                <Text as="p" weight="regular" size="2" className="text-left">
+                  {status}
+                </Text>
+                <Switch
+                  variant="soft"
+                  size="2"
+                  checked={isBlockedOutDaysSwitchEnabled}
+                  onCheckedChange={handleBlockedOutSwitchChange}
+                />
+              </Flex>
+
+              {isBlockedOutDaysSwitchEnabled && (
+                <>
+                  <Flex align="center" justify="between" className="my-4">
+                    <Flex key={id} align="center">
+                      <Select.Root size="1">
+                        <Select.Trigger
+                          placeholder="Wed, 12 Dec 2024"
+                          className="w-[140px]"
+                        />
+                        <Select.Content variant="soft">
+                          <Select.Group>
+                            <Select.Label>Fruits</Select.Label>
+                            <Select.Item value="orange">Orange</Select.Item>
+                            <Select.Item value="apple">Apple</Select.Item>
+                            <Select.Item value="grape" disabled>
+                              Grape
+                            </Select.Item>
+                          </Select.Group>
+                        </Select.Content>
+                      </Select.Root>
+                      <Text as="p" size="1" weight="regular" className="px-6">
+                        To
+                      </Text>
+                      <Select.Root size="1">
+                        <Select.Trigger
+                          className="w-[140px]"
+                          placeholder="Mon, 17 Dec 2024"
+                        />
+                        <Select.Content>
+                          <Select.Group>
+                            <Select.Label>Fruits</Select.Label>
+                            <Select.Item value="orange">Orange</Select.Item>
+                            <Select.Item value="apple">Apple</Select.Item>
+                            <Select.Item value="grape" disabled>
+                              Grape
+                            </Select.Item>
+                          </Select.Group>
+                        </Select.Content>
+                      </Select.Root>
+                    </Flex>
+
+                    <IconButton
+                      style={{
+                        border: "1px solid var(--border-gray)",
+                      }}
+                      className="bg-transparent text-neutral_11"
+                      size="1"
+                    >
+                      <TrashIcon />
+                    </IconButton>
+                  </Flex>
+                  <Button
+                    style={{
+                      border: "1px solid var(--border-gray)",
+                    }}
+                    size="1"
+                    className="text-sm font-medium bg-transparent text-neutral_11 mb-4"
+                  >
+                    <PlusIcon /> Add Blockout Dates
+                  </Button>
+                </>
+              )}
+            </Box>
+          );
+        })}
+      </div>
+    </Box>
+  </Flex>
+</Flex>;
